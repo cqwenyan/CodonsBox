c8fde1baa92bea315ca8948d89975b68d8d59504
WillYan <253364208@qq.com>
2017-02-07 22:40:37 +0800

    PlusInputcomplete

--- src/Core/MainMenu.java
+++ src/Core/MainMenu.java
@@ -15,6 +15,8 @@ import javax.swing.JTextField;
 import javax.swing.JButton;
 import javax.swing.LayoutStyle.ComponentPlacement;
 
+
+
 import java.awt.Toolkit;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
@@ -30,6 +32,10 @@ import java.awt.event.MouseEvent;
 import java.io.File;
 import java.awt.Font;
 
+import javax.swing.SwingConstants;
+
+import java.awt.GridLayout;
+
 
 public class MainMenu extends JFrame {
 
@@ -37,6 +43,11 @@ public class MainMenu extends JFrame {
 	private JTextField txtFilePath;
 	public static File inputFileName;
 	private static MainMenu frame;
+	private static JTextField txtA;
+	private static JPanel panel_1;
+	private static JLabel lblNewLabel;
+	public static enum PulsType{No,CircosGC,AA};
+	public static PulsType testType = PulsType.No;
 //	Controller controllerInstance = Controller.getController();
 	
 	public static MainMenu getMainMenu(){
@@ -75,8 +86,8 @@ public class MainMenu extends JFrame {
 		
 		final JComboBox comboBox = new JComboBox();
 		comboBox.addItem("GC123");
-		comboBox.addItem("Cuter");
-		comboBox.addItem("军官证");
+		comboBox.addItem("CircosGC");
+		comboBox.addItem("AA");
 		
 		
 		txtFilePath = new JTextField();
@@ -130,37 +141,77 @@ public class MainMenu extends JFrame {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
+		
+		JPanel panel = new JPanel();
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
+					.addGap(39)
+					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 345, GroupLayout.PREFERRED_SIZE)
+						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 192, GroupLayout.PREFERRED_SIZE)
+						.addComponent(txtFilePath, GroupLayout.PREFERRED_SIZE, 373, GroupLayout.PREFERRED_SIZE))
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
 						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(39)
-							.addComponent(txtFilePath, GroupLayout.PREFERRED_SIZE, 373, GroupLayout.PREFERRED_SIZE)
 							.addGap(35)
-							.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addGap(48)
-							.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, 192, GroupLayout.PREFERRED_SIZE)
-							.addGap(207)
-							.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 121, Short.MAX_VALUE)))
+							.addComponent(btnNewButton, GroupLayout.DEFAULT_SIZE, 126, Short.MAX_VALUE))
+						.addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 161, Short.MAX_VALUE))
 					.addContainerGap())
 		);
 		gl_contentPane.setVerticalGroup(
 			gl_contentPane.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+					.addContainerGap(29, Short.MAX_VALUE)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
 						.addComponent(txtFilePath, GroupLayout.PREFERRED_SIZE, 26, GroupLayout.PREFERRED_SIZE)
 						.addComponent(btnNewButton, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
 					.addGap(37)
 					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-						.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 82, GroupLayout.PREFERRED_SIZE))
-					.addGap(128))
+						.addComponent(btnNewButton_1, GroupLayout.PREFERRED_SIZE, 82, GroupLayout.PREFERRED_SIZE)
+						.addGroup(gl_contentPane.createSequentialGroup()
+							.addComponent(comboBox, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+							.addGap(31)
+							.addComponent(panel, GroupLayout.PREFERRED_SIZE, 107, GroupLayout.PREFERRED_SIZE)))
+					.addGap(48))
 		);
+		panel.setLayout(new GridLayout(1, 0, 0, 0));
+		
+		//
+		panel_1 = new JPanel();
+		panel_1.setVisible(false);
+		panel_1.setBackground(UIManager.getColor("Button.light"));
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		lblNewLabel = new JLabel("片段大小(bp)");
+		lblNewLabel.setFont(new Font("宋体", Font.BOLD, 15));
+		lblNewLabel.setEnabled(false);
+		lblNewLabel.setBounds(14, 13, 317, 27);
+		panel_1.add(lblNewLabel);
+		
+		txtA = new JTextField();
+		txtA.setText("0");
+		txtA.setBounds(14, 53, 317, 27);
+		panel_1.add(txtA);
+		txtA.setColumns(10);
 		contentPane.setLayout(gl_contentPane);
+	}
+	
+	//刷新plus区域
+	public static void RefreshPlus(PulsType pulsType){
+		switch (pulsType) {
+		case CircosGC:
+			panel_1.setVisible(true);
+			
+			break;
+		case AA:
+			panel_1.setVisible(true);
+			lblNewLabel.setText("请输入A");
+			break;
+		default:
+			break;
+		}
 	}
 	
 	public void setFileNameIntextfield(){
@@ -168,5 +219,7 @@ public class MainMenu extends JFrame {
 		txtFilePath.setText(controllerInstance.getFilePath()+controllerInstance.getFileName());
 	}
 	
-	
+	public static String getPlusfield(){
+			return txtA.getText();
+	}
 }
--- src/function/CircosGC.java
+++ src/function/CircosGC.java
@@ -1,8 +1,15 @@
 package function;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 import core.CleanBreak;
+import core.MainMenu;
+import core.MainMenu.PulsType;
 import core.WorkComplete;
 
 public class CircosGC implements Operation {
@@ -10,19 +17,53 @@ public class CircosGC implements Operation {
 	@Override
 	public void workNew(String path, String fileName) {
 		// TODO Auto-generated method stub
+		MainMenu.RefreshPlus(PulsType.CircosGC);
 
-		CleanBreak cleanBreak = new CleanBreak();
-		cleanBreak.cleanLineBreak(path, fileName);
-		String cmd = "cmd /F java 1221.jar";//System.getProperty()
-		Runtime rt = Runtime.getRuntime(); 
-		try {
-			Process proc = rt.exec(cmd);
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+		if(!MainMenu.getPlusfield().equals("0")){
+			System.out.println(MainMenu.getPlusfield()+"1");
+//		CleanBreak cleanBreak = new CleanBreak();
+//		cleanBreak.cleanLineBreak(path, fileName);
+			genIterator(path+fileName, Integer.parseInt(MainMenu.getPlusfield()),"CodonsBoxGC");
+//		cleanBreak.deleteTempFile(path, fileName);
+			new WorkComplete();
+			
 		}
-		cleanBreak.deleteTempFile(path, fileName);
-		new WorkComplete();
 	}
 
+	public static void genIterator(String inputFileName,  int frameSize,String chromosomeName) {
+		try {
+			File writeTo = new File(
+					inputFileName+"_result.fasta");
+			BufferedWriter bw = new BufferedWriter(new FileWriter(writeTo, true));
+			String tempString = null;
+//			StringBuffer stringBuf = new StringBuffer();
+			StringBuilder stringBud = new StringBuilder();
+			
+
+			File file = new File(inputFileName);
+			BufferedReader reader = new BufferedReader(new FileReader(file));
+			int counter = 0;
+			while ((tempString = reader.readLine()) != null) {
+				stringBud.append(tempString.trim());
+				
+				while(stringBud.length()>=frameSize){
+					int GCcounter = 0;
+					for (int i = 0; i < frameSize; i++) {
+						if(("c".equals(stringBud.subSequence(i, i+1)))||("C".equals(stringBud.subSequence(i, i+1)))||("g".equals(stringBud.subSequence(i, i+1)))||("G".equals(stringBud.subSequence(i, i+1)))){
+							GCcounter++;
+						}
+					}
+					bw.write(chromosomeName+" "+counter+" "+(counter+frameSize)+" "+((float)GCcounter)/frameSize+"\n");
+					counter+=frameSize;
+					stringBud.delete(0, frameSize);
+				}
+				
+			}
+			bw.flush();
+			reader.close();
+			bw.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }
